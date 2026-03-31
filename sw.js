6477c364962dbf4ada6cb7b59b1aa69a8fb45168
Spirit-infinity <bababigshop@hotmail.fr>
2026-03-31 13:41:27 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const SPIRIT_SW_CACHE = 'spirit-sw-v1';
+
+self.addEventListener('install', function () {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (k) {
+          if (k !== SPIRIT_SW_CACHE) return caches.delete(k);
+          return Promise.resolve();
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
+  );
+});
